26841af9820e6c501b9da7145ae718cf82115db1
unknown <harisamjad980@gmail.com>
2021-09-27 17:42:53 +0500

    d

--- resources/views/admin/setting/about_us.blade.php
+++ resources/views/admin/setting/about_us.blade.php
@@ -31,17 +31,33 @@
                             <input type="file" id="about_us_image" name="about_us_image" data-default-file="{{ asset(setting("about_us_image")) }}" class="form-control dropify">
                         </div>
                         <div class="form-group">
+                            <label for="logo">About us Image 2*</label>
+                            <input type="file" id="about_us_image2" name="about_us_image2" data-default-file="{{ asset(setting("about_us_image2")) }}" class="form-control dropify">
+                        </div>
+                        <div class="form-group">
+                            <label for="logo">About us Image 3*</label>
+                            <input type="file" id="about_us_image3" name="about_us_image3" data-default-file="{{ asset(setting("about_us_image3")) }}" class="form-control dropify">
+                        </div>
+                        <div class="form-group">
                             <label for="name">About us section title *</label>
                             <input type="text"  name="about_sec_title" value="{{ setting('about_sec_title') }}" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">About us section title 2*</label>
+                            <input type="text"  name="about_sec_title2" value="{{ setting('about_sec_title2') }}" class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="name">About us section details *</label>
                             <textarea  name="about_sec_detail"  class="form-control ckeditor"> {{ setting('about_sec_detail') }}</textarea>
                         </div>
                         <div class="form-group">
+                            <label for="name">About us section details 2*</label>
+                            <textarea  name="about_sec_detail2"  class="form-control ckeditor"> {{ setting('about_sec_detail2') }}</textarea>
+                        </div>
+                        {{-- <div class="form-group">
                             <label for="logo">About us Video *</label>
                             <input type="file" id="about_video" name="about_video" data-default-file="{{ asset(setting("about_video")) }}" class="form-control dropify">
-                        </div>
+                        </div> --}}
 
                         <div class="form-group">
                             <button type="submit" class="btn btn-purple waves-effect waves-light">Save</button>
--- resources/views/front/about_us.blade.php
+++ resources/views/front/about_us.blade.php
@@ -2,6 +2,14 @@
 
 @section('title', 'About Us')
 
+@section('css')
+    <style>
+        .section {
+            padding: 60px 0px;
+        }
+    </style>
+@endsection
+
 @section('content')
 
 @include('front.components.pages_banner')
@@ -12,24 +20,52 @@
         <div class="row align-items-center" id="counter">
             <div class="col-md-6">
                 <img src="{{ asset(setting('about_us_image')) }}" class="img-fluid" alt="">
+                <img src="{{ asset(setting('about_us_image2')) }}" class="img-fluid" alt="">
             </div><!--end col-->
 
             <div class="col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
                 <div class="ms-lg-4">
-                    {{-- <div class="d-flex mb-4">
-                        <span class="text-primary h1 mb-0"><span class="counter-value display-1 fw-bold" data-target="15">0</span>+</span>
-                        <span class="h6 align-self-end ms-2">Years <br> Experience</span>
-                    </div> --}}
                     <div class="section-title">
                         <h4 class="title mb-4">{{ setting('about_sec_title') }}</h4>
-                        {!! setting('about_sec_detail') !!}
-                        {{-- <a href="javascript:void(0)" class="btn btn-primary mt-3">Contact us</a> --}}
+                        <p class="text-muted">We are the neighbors…..</p>
+                        <p class="text-muted">
+                            {!! setting('about_sec_detail') !!}
+                            {{-- We Love to share our thought’s and vision with them we should talk about We Love to share our thought’s and vision with them how we can create a better community.<br>
+                            How we can live better? How can we be more active? and yes We are working making a bridge. We will do it, for a beautiful world, we are here. <br>
+                            We are Co-operating every way <br>
+                            We like to hear from them <br>
+                            We ask if they need help <br>
+                            We join them to discuss <br>
+                            We connect them to build a good neighborhood, we know we can be more stronger only when we are together and that’s why We need to communicate and this is the power <br> --}}
+                        </p>
                     </div>
                 </div>
             </div><!--end col-->
         </div><!--end row-->
     </div><!--end container-->
 </section>
+
+<section class="section">
+    <div class="container">
+        <div class="row align-items-center" id="counter">
+            <div class="col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
+                <div class="ms-lg-4">
+                    <div class="section-title">
+                        <h4 class="title mb-4">{{ setting('about_sec_title2') }}</h4>
+                        <p class="text-muted">
+                            <span class="text-primary fw-bold">Free shopps</span> {!! setting('about_sec_detail2') !!}
+                        </p>
+                    </div>
+                </div>
+            </div><!--end col-->
+
+            <div class="col-md-6">
+                <img src="{{ asset(setting('about_us_image3')) }}" class="img-fluid" alt="">
+            </div><!--end col-->
+        </div><!--end row-->
+    </div><!--end container-->
+</section>
+
 <section class="section bg-light">
     <div class="container">
         <div class="row justify-content-center">
@@ -37,7 +73,7 @@
                 <div class="section-title text-center mb-4 pb-2">
                     <h6 class="text-primary">Work Process</h6>
                     <h4 class="title mb-4">How do we works ?</h4>
-                    <p class="text-muted para-desc mx-auto mb-0">Start using <span class="text-primary fw-bold">Free shopps</span> , help others and get helped.</p>
+                    <p class="text-muted para-desc mx-auto mb-0">Start using <span class="text-primary fw-bold">Freeshopps</span> , help others and get helped.</p>
                 </div>
             </div><!--end col-->
         </div><!--end row-->
@@ -50,8 +86,8 @@
                     </div>
 
                     <div class="card-body">
-                        <h5 class="text-dark">Add to Cart</h5>
-                        <p class="text-muted mb-0">Like something ! Just add it to cart.</p>
+                        <h5 class="text-dark">Create Account</h5>
+                        <p class="text-muted mb-0">It’s important for us to communicate with you , your safety, security and privacy is our first priority, please make sure your information is accurate. We are always happy to answer your any questions you may have.</p>
                     </div>
                 </div>
             </div><!--end col-->
@@ -63,8 +99,8 @@
                     </div>
 
                     <div class="card-body">
-                        <h5 class="text-dark">Proceed to Pay</h5>
-                        <p class="text-muted mb-0">Pay the processing Fee.</p>
+                        <h5 class="text-dark">List your Stuff</h5>
+                        <p class="text-muted mb-0">We appreciate your support and can’t be successful without your participation, please encourage others as you posting yours together we always be more strong.</p>
                     </div>
                 </div>
             </div><!--end col-->
@@ -76,51 +112,13 @@
                     </div>
 
                     <div class="card-body">
-                        <h5 class="text-dark">Get your Item</h5>
-                        <p class="text-muted mb-0">Get your order number and contact Donar to receive Item..</p>
+                        <h5 class="text-dark">Please Response</h5>
+                        <p class="text-muted mb-0">Whenever any neighbor inquire your item we will notify you with a confirmation code which you will be matching with the person who will be picking the item , with your help and co-operation we can build an unique world community.</p>
                     </div>
                 </div>
             </div><!--end col-->
         </div><!--end row-->
     </div><!--end container-->
 </section>
-<section class="section bg-light pt-0">
-    {{-- <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12 text-center">
-                <div class="video-solution-cta position-relative" style="z-index: 1;">
-                    <div class="position-relative">
-                        <img src="{{ asset('freeshopps-banner.png') }}" class="img-fluid rounded-md shadow-lg" alt="">
-                        <div class="play-icon">
-                            <a href="#!" data-type="youtube" data-id="yba7hPeTSjk" class="play-btn lightbox">
-                                <i class="mdi mdi-play text-primary rounded-circle bg-white shadow-lg"></i>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div><!--end col-->
-        </div><!--end row -->
-    </div><!--end container--> --}}
-    <div class="offset-lg-3 col-lg-6 mt-4 pt-2 mt-lg-0 pt-lg-0">
-        <div class="card border-0 shadow rounded ms-lg-4 overflow-hidden">
-            <div class="d-flex p-2 bg-light justify-content-between align-items-center">
-                <div>
-                    <a href="javascript:void(0)" class="text-danger"><i class="mdi mdi-circle"></i></a>
-                    <a href="javascript:void(0)" class="text-warning"><i class="mdi mdi-circle"></i></a>
-                    <a href="javascript:void(0)" class="text-success"><i class="mdi mdi-circle"></i></a>
-                </div>
-
-                <small class="fw-bold"><i class="mdi mdi-circle-medium text-success"></i> FreeShopps</small>
-            </div>
-            <div class="bg-light px-2 position-relative">
-                <video class="w-100 rounded" controls="" loop="">
-                    <source src="{{ setting('about_video') }}" type="video/mp4">
-                </video>
-            </div>
-        </div>
-    </div>
-</section>
-
 @endsection
 
